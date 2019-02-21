e10dac9089f1cc9ab0ce2b650d199ec0a38cb81a
Litterial <yungice12@gmail.com>
2019-02-21 15:16:33 -0600

    completed w/full comments

--- cwProject/cwApp/urls.py
+++ cwProject/cwApp/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
+    #Books paths
     path('',views.index,name="index"),  #home page
     path('newBook1/',views.newBook1,name="newBook1"),
     path('newBook2/',views.newBook2,name="newBook2"),
     path('allBookNames/',views.allBookNames,name="allBooksNames"),
-    path('dateLessThan',views.dateLessThan,name="dateGreaterThan"),
-
+    path('dateLessThan/',views.dateLessThan,name="dateGreaterThan"),
+   #Car views
     path('newCar1/',views.newCar1,name="newCar1"),
     path('newCar2/',views.newCar2,name='newCar2'),
     path('allYears/',views.allYears,name='allYears'),
-    path('post2010',views.post2010,name='post2010')
+    path('post2010/',views.post2010,name='post2010')
 ]
--- cwProject/cwApp/views.py
+++ cwProject/cwApp/views.py
@@ -9,7 +9,7 @@ def index(request):  #test to see if my server is up
     return HttpResponse("Testing")
 
 def newBook1(request): #creates new entry for book 1
-    classBook1= Book(name='Moby Dick', page=927, genre='adventure-fiction',publishDate="2017-10-18")
+    classBook1= Book(name='Moby Dick', page=927, genre='adventure-fiction',publishDate="2019-10-18")
     classBook1.save()
     return HttpResponse(classBook1)
 
@@ -30,8 +30,11 @@ def allBookNames (request): #returns all books
 
 #
 def dateLessThan(request): #books after Jan 2,2018
+    dates=''
     datesYounger01012018= Book.objects.filter(publishDate__gte= '2018-01-01')
-    return HttpResponse(datesYounger01012018)
+    for eachElement in datesYounger01012018:
+        dates+=str(eachElement.name) + '<br>'
+    return HttpResponse(dates)
 
 def newCar1(request): #create car 1
     classCar1=Car(make='Nissan',model="Altura",year="2009")
@@ -48,11 +51,14 @@ def allYears(request): #returns all years
     carYears=''
     totalYears=Car.objects.all()
     for eachElement in totalYears:
-        carYears+=eachElement.year + '<br>'
+        carYears+= str(eachElement.year) + '<br>'
     return HttpResponse(carYears)
 
 def post2010(request): #returns after post 2010
+    listofcars=''
     Over2010=Car.objects.filter(year__gt=2010)
-    return HttpResponse(Over2010)
+    for eachElement in Over2010:
+        listofcars+= str(eachElement.make) + '<br>'
+    return HttpResponse(listofcars)
 
 
